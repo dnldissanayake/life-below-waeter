d27a7f05a192d856cc37e0aa30b4553fd71295bf
R.M.V.D Jayathilaka <86566193+rmvdjayathilaka@users.noreply.github.com>
2021-07-14 22:06:47 +0530

    Update login3.php

--- login3.php
+++ login3.php
@@ -1,51 +1,42 @@
 <?php
+    $uName = filter_input(INPUT_POST, 'username');
+    $password = filter_input(INPUT_POST, 'password');
 
-session_start();
+    $errorMsg = "";
+    $isValid = TRUE;
 
-    include("connection.php");
-    include("functions.php");
-
-    if($_SERVER['REQUEST_METHOD'] == "POST")
-    {
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-        
-
-       if(!empty($username) && !empty($password) )
-          {
-            //read from database
-           
-            $query = "select * from users where username= '$username' ;";
-        
-            $result= mysqli_query($con, $query);
-
-            if($result)
-            {
-                    if($result && mysqli_num_rows($result) > 0)
-                {
-                    $user_data = mysqli_fetch_assoc($result);
-        
-                    if($user_data['password']=== $password)
-                    {
-                        $_SESSION['user_id'] =$user_data['user_id'];
-                      
-                        header("Location: index.html");
-                        die;
-                    }
-
-                }
-            }
-           
-            header("Location: wrong.html");
-            die;
-                   
-        } else
-       
-        {
-            
-            header("Location: wrong.html");
-            die;
-           
-        }
+    if(empty($uName)) {
+        $errorMsg .= "Please enter your user name.<br>";
+        $isValid = FALSE;
     }
-   ?> 
+    if (empty($password)){
+        $errorMsg .= "Please enter your password.<br>";
+        $isValid = FALSE;
+    }
+
+    if($isValid === FALSE){
+        header("Location: ./error.php?errorMsg=$errorMsg");
+        exit();
+    }
+    else{
+        require('./connection.php');
+        $query = 'select id from users where username=:uname and password=:pass';
+        $statement = $database->prepare($query);
+        $statement->bindValue(':uname',$uName);
+        $statement->bindValue(':pass',$password);
+        $statement->execute();
+        $user_id_result = $statement->fetch();
+        $statement->closeCursor();
+
+        if($user_id_result === FALSE){
+            $errorMsg = "The account was not found. Please try a different username or password.<br>";
+            header("Location: ./error.php?errorMsg=$errorMsg");
+            exit();
+        }
+        
+        session_start();
+        $_SESSION['user_id'] = $user_id_result['id'];
+
+        header('Location: ./myAccount.php');
+    }
+?>
